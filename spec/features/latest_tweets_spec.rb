c7f89a7fe01b5415e5fac97f804e8b021d544034
mcberros <mcberros@gmail.com>
2014-08-14 16:03:15 +0200

    Fixed indentation

--- spec/features/latest_tweets_spec.rb
+++ spec/features/latest_tweets_spec.rb
@@ -8,13 +8,13 @@ describe 'latest tweets' do
 		it 'print mentions of user name' do
 			initial_cassette_for_mentions
 			change_mentions_from_twitter_cassette
-				VCR.use_cassette('mentions_from_twitter') do
-					twitter_service = TwitterAccess.new
-					mentions = twitter_service.get_mentions_to_user('mcberros')
-					expect(STDOUT).to receive(:puts).with("2014-08-07 22:04:32 +0200 @aitorciki Pregunta a @eamodeorubio @mcberros o @porras, que ellos s\u00ED que est\u00E1n \"viajados\" por Berl\u00EDn :-)")
-		   		expect(STDOUT).to receive(:puts).with("2014-08-06 18:18:54 +0200 every time i had @mcberros in front of my camera she was smiling. but i like that shot best: https://t.co/G4skfFOVih #ec14 #eurucamp")
-					Printer.new.print_tweets(mentions)
-				end
+			VCR.use_cassette('mentions_from_twitter') do
+				twitter_service = TwitterAccess.new
+				mentions = twitter_service.get_mentions_to_user('mcberros')
+				expect(STDOUT).to receive(:puts).with("2014-08-07 22:04:32 +0200 @aitorciki Pregunta a @eamodeorubio @mcberros o @porras, que ellos s\u00ED que est\u00E1n \"viajados\" por Berl\u00EDn :-)")
+	   		expect(STDOUT).to receive(:puts).with("2014-08-06 18:18:54 +0200 every time i had @mcberros in front of my camera she was smiling. but i like that shot best: https://t.co/G4skfFOVih #ec14 #eurucamp")
+				Printer.new.print_tweets(mentions)
+			end
 		end
 	end
 

a549af4dfdc54b0ce4968a43c9fe35877f206250
mcberros <mcberros@gmail.com>
2014-08-14 18:39:35 +0200

    Added new lines

--- spec/features/latest_tweets_spec.rb
+++ spec/features/latest_tweets_spec.rb
@@ -11,8 +11,10 @@ describe 'latest tweets' do
 			VCR.use_cassette('mentions_from_twitter') do
 				twitter_service = TwitterAccess.new
 				mentions = twitter_service.get_mentions_to_user(Helpers::EXPECTED_EXISTING_USER_NAME)
+
 				expect(STDOUT).to receive(:puts).with("2014-08-07 22:04:32 +0200 @aitorciki Pregunta a @eamodeorubio @mcberros o @porras, que ellos s\u00ED que est\u00E1n \"viajados\" por Berl\u00EDn :-)")
 	   		expect(STDOUT).to receive(:puts).with("2014-08-06 18:18:54 +0200 every time i had @mcberros in front of my camera she was smiling. but i like that shot best: https://t.co/G4skfFOVih #ec14 #eurucamp")
+
 				Printer.new.print_tweets(mentions)
 			end
 		end
